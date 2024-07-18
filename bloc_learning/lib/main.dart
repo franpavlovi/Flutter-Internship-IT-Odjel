046f1cebb2802663a1be632d3406aa137d53be81
Fran Pavlović <fljubuski@gmail.com>
2024-07-18 12:52:43 +0200

    BlocBuilder

--- bloc_learning/lib/main.dart
+++ bloc_learning/lib/main.dart
@@ -12,41 +12,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      //providamo svim child widgetima
       create: (context) => BrojacCubit(),
       child: MaterialApp(
         title: 'BLoC Learning',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'BLoC Learning',
-              style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 120),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white,
+        home: BrojacScreen(),
+      ),
+    );
+  }
+}
+
+class BrojacScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'BLoC Learning',
+          style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 120),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        BlocProvider.of<BrojacCubit>(context).smanji(); // povezivanje funkcije i iconbuttona
+                      },
+                      icon: const Icon(Icons.remove),
                     ),
-                    onPressed: () {},
-                    child: const Row(children: [
-                      Icon(Icons.remove),
-                      Spacer(),
-                      Text('BROJ'),
-                      Spacer(),
-                      Icon(Icons.add),
-                    ]),
-                  ),
-                )
-              ],
+                    const Spacer(),
+                    BlocBuilder<BrojacCubit, BrojacState>(
+                      builder: (context, state) {
+                        return Text(state.brojac.toString());
+                      },
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {
+                        BlocProvider.of<BrojacCubit>(context).povecaj(); // povezivanje funkcije i iconbuttona
+                      },
+                      icon: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
